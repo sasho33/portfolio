490538098f11ce052edb49159fbd77c1bd969055
sasho33 <sasho3333333@gmail.com>
2024-10-24 22:51:19 +0100

    dark mode on local storage and user preferences

--- js/main.js
+++ js/main.js
@@ -1,10 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
   const btnDarkMode = document.querySelector('.dark-mode-btn');
 
-  if (btnDarkMode) {
-    btnDarkMode.addEventListener('click', () => {
-      btnDarkMode.classList.toggle('dark-mode-btn--active');
-      document.body.classList.toggle('dark');
-    });
+  // 1. checking the user's preference for dark mode using the matchMedia method
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.body.classList.add('dark');
+    btnDarkMode.classList.add('dark-mode-btn--active');
   }
+
+  // checking the local storage if it has a value of dark mode set to dark or light
+  if (localStorage.getItem('dark-mode') === 'dark') {
+    document.body.classList.add('dark');
+    btnDarkMode.classList.add('dark-mode-btn--active');
+  } else if (localStorage.getItem('dark-mode') === 'light') {
+    document.body.classList.remove('dark');
+    btnDarkMode.classList.remove('dark-mode-btn--active');
+  }
+
+  // check if the user's preference for dark mode changes
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+    if (e.matches) {
+      document.body.classList.add('dark');
+      btnDarkMode.classList.add('dark-mode-btn--active');
+    } else {
+      document.body.classList.remove('dark');
+      btnDarkMode.classList.remove('dark-mode-btn--active');
+    }
+  });
+
+  // adding an event listener to the dark mode button
+  btnDarkMode.addEventListener('click', () => {
+    btnDarkMode.classList.toggle('dark-mode-btn--active');
+    const isDark = document.body.classList.toggle('dark');
+
+    if (isDark) {
+      localStorage.setItem('dark-mode', 'dark');
+    } else {
+      localStorage.setItem('dark-mode', 'light');
+    }
+  });
 });
