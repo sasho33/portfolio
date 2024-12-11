e5ef6dafec4e2e2d5caf1da938c50b6011516907
sasho33 <sasho3333333@gmail.com>
2024-12-11 15:38:30 +0000

    commit

--- src/components/btnDarkMode/BtnDarkMode.js
+++ src/components/btnDarkMode/BtnDarkMode.js
@@ -30,7 +30,7 @@ export default function BtnDarkMode() {
       const newColorScheme = event.matches ? 'dark' : 'light';
       setDarkMode(newColorScheme);
     });
-  }, []);
+  }, [setDarkMode]);
 
   return (
     <button ref={btnRef} onClick={toggleDarkMode} className="dark-mode-btn">
